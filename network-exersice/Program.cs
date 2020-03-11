3bc567de579c2ecc559f02203daf74256a0d53c9
Junichi <junichi_hashimoto@hotmail.co.jp>
2020-03-11 06:43:28 +0000

    create project

---
+++ network-exersice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace network_exersice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
